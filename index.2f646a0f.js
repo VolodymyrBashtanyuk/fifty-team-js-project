fdc94d70a24da9afd61ca713245294186a9b0445
VolodymyrBashtanyuk <96777965+VolodymyrBashtanyuk@users.noreply.github.com>
2022-08-25 06:39:48 +0000

    Deploying to gh-pages from @ VolodymyrBashtanyuk/fifty-team-js-project@dbce6db76b98fbb88313f18cbea7ebe264e216d0 🚀

--- index.2f646a0f.js
+++
@@ -1,2 +0,0 @@
-const t=new class{async fetchMovies(){const t=await fetch(`https://api.themoviedb.org/3/list/${this.page}?api_key=9d0cbfdf54c935b25322d86e1e3b7dd0`);return await t.json()}nextPage(){this.page+=1}resetPage(){this.page=1}constructor(){this.page=1}};!async function(){try{const e=await t.fetchMovies();console.log(e)}catch(t){console.log(t)}}();
-//# sourceMappingURL=index.2f646a0f.js.map
