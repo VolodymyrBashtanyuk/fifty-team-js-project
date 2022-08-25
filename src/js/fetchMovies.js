dbce6db76b98fbb88313f18cbea7ebe264e216d0
VOLODYMYR BASHTANYUK <96777965+VolodymyrBashtanyuk@users.noreply.github.com>
2022-08-25 09:39:19 +0300

    add correction in fetch

--- src/js/fetchMovies.js
+++ src/js/fetchMovies.js
@@ -4,10 +4,10 @@ export default class theMovieDbApi {
     }
 async fetchMovies(){
      
-    const BASE_URL='https://api.themoviedb.org/3/list/';
+    const BASE_URL='https://api.themoviedb.org/3/movie/popular';
     const KEY='9d0cbfdf54c935b25322d86e1e3b7dd0';
       
-    const fetchRequest = await fetch(`${BASE_URL}${this.page}?api_key=${KEY}`);
+    const fetchRequest = await fetch(`${BASE_URL}?api_key=${KEY}`);
       const data = await fetchRequest.json();
  
     return data;
