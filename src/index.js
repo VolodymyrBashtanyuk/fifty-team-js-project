a06d141708af3bb7c34dc8063938d0f921370271
Darya <daryabobur@gmail.com>
2022-08-28 14:35:18 +0300

    Додає підключення модалки

--- src/index.js
+++ src/index.js
@@ -3,22 +3,36 @@ import insertCreatedObject from './js/createOneObject'
 import spinner from './js/preLoader'
 
 
+import {createPagination} from "./js/createPagination"
+
+
 import {createPagination, getCurrentPageLs} from "./js/createPagination"
+
 
 const movieDbApi = new theMovieDbApi();
 
+const cardOneFilm = document.querySelector('.gallery');
+cardOneFilm.addEventListener('click', oneMovies);
+import openCardFilm from './js/openCardFilm'
+import createdCardFilm from "./js/markUpModal";
 
 async function movies(){
     spinner.startSpinner();
     try{
-        const response = await movieDbApi.fetchMovies();
-        const genreResponse = await movieDbApi.fetchGenres();
-        console.log(response);
-        console.log(genreResponse);
+       const response = await movieDbApi.fetchMovies();
+       const genreResponse = await movieDbApi.fetchGenres();
+       console.log(response);
+       console.log(genreResponse);
+       insertCreatedObject(response.results)
+       spinner.removeSpinner();
+
+     createPagination(response)
+
         insertCreatedObject(response.results)
         if(response.total_pages > 1) createPagination(response)
         spinner.removeSpinner();
         
+
     }catch(error){
         console.log(error)
     };
@@ -26,3 +40,13 @@ async function movies(){
 
 movieDbApi.setPage(getCurrentPageLs())        
 movies();
+
+
+
+async function oneMovies(e) {
+    const id = e.target.parentNode.parentNode.id;
+    const oneMovieResponse = await movieDbApi.fetchOneMovie(id);
+    createdCardFilm(oneMovieResponse);
+    console.log(oneMovieResponse)
+    };
+
--- src/js/openCardFilm.js
+++ src/js/openCardFilm.js
@@ -1,8 +1,8 @@
 const cardFilm = document.querySelector('.gallery');
 
 const refsModal = {
-    modal: document.querySelector('.modal'),
-    backdrop: document.querySelector('.backdrop'),
+    modal: document.querySelector('.modal-movie'),
+    backdrop: document.querySelector('.backdrop-modal'),
     };
 
 
@@ -16,7 +16,7 @@ function openCardFilm (e) {
         return;
     } 
 
-refsModal.backdrop.classList.add('is-visible');
+refsModal.backdrop.classList.add('is-open');
 document.addEventListener('keydown', closeModalEsc);
 document.removeEventListener('click', closeModalEsc);
 
@@ -26,13 +26,14 @@ function closeModalEsc(evt) {
 
     if (evt.code === "Escape") {
 
-    refsModal.backdrop.classList.remove('is-visible');
+    refsModal.backdrop.classList.remove('is-open');
     document.removeEventListener('keydown', closeModalEsc);
     }
   };
 
 function closeModalClick() {
-    refsModal.backdrop.classList.remove('is-visible');
+    refsModal.backdrop.classList.remove('is-open');
     document.removeEventListener('keydown', closeModalEsc);
     document.removeEventListener('click', closeModalClick);
 };
+
