e08dbc545c2a3457142d1ce99f633121192f484e
Olena-Zhyzhko <asil198609@gmail.com>
2022-08-29 19:30:39 +0300

    додає кнопку ще

--- src/js/createOneObject.js
+++ src/js/createOneObject.js
@@ -1,25 +1,38 @@
 const refs = {
     gallery:document.querySelector('.gallery')
 }
+// const btnEtc = document.addEventListener(".btn__etc");
 
 import { filmGenre } from './genre'
 
 const noImg ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT6UB-bP8Q1VOTxmMLs-1v1d6LN4b7H7U3Ng&usqp=CAU';
 const imageURL = "https://image.tmdb.org/t/p/w500";
-function createOneObject(film){
+
+function createOneObject(film) {
   const { title, genre_ids, poster_path, release_date, id } = film;
-  const genreInCard = filmGenre(genre_ids);
+  const arrayGenreInCard = filmGenre(genre_ids);
+  const genreInString = arrayGenreInCard.join(", ");
+  const genreInCard = smartGenre(arrayGenreInCard, genreInString);
+  // const btnHide = btnEtc.classList.add(hide);
+  // if (arrayGenreInCard.lenght <= 3) {   
+  //   btnEtc.classList.add(hide);
+  // }
 
 return `
 <li class="film-card" id = ${id}>
-  <a href=''></a>
-  ${poster_path ? 
-    `<img class="poster" src="${imageURL}${poster_path}" alt="${title}"  />` : 
-    `<img class="poster" src=${noImg} alt="${title}" />`
-}
+
+  <a href=''>
+    <div class="poster__thumb">
+      ${poster_path ? 
+      `<img class="poster" src="${imageURL}${poster_path}" alt="${title}"  />` : 
+      `<img class="poster" src=${noImg} alt="${title}" />`
+      }
+    </div>
     <p class="info-title">${title ?? ''}</p>
     <div class="info-title info-item">
-      <p class="">${genreInCard ?? ''}
+      <p class="">${genreInCard ? genreInCard : ''}
+      <button class="btn__etc ${arrayGenreInCard.length <= 3 ? 'hide' : ''}">...</button>
+
       </p>
       <p class="">${release_date.slice(0,4) ?? ''}
       </p>
@@ -28,7 +41,9 @@ return `
 </li>
 `};
 
-
+// if (arrayGenreInCard.lenght <= 3) {   
+//     btnEtc.classList.add(hide);
+//   }
 
 function generateMarkup (arrayImages){
 
@@ -41,3 +56,14 @@ return arrayImages.reduce((acc,film)=>acc + createOneObject(film),"");
  
 
 };
+
+function smartGenre(arrayGenreInCard, genreInString) {
+  if (arrayGenreInCard.length <= 3) {   
+    return genreInString;
+  }
+    const cutGenre = arrayGenreInCard.slice(0, 3).join(", ");
+    return cutGenre; 
+}
+
+// function isH
+
