5a85dcea504dda3b275a8f8d22321c547d1f9b4c
OspanovaVita <vitaospanova@ukr.net>
2022-08-29 20:44:22 +0300

    вернулась к исходникам

--- src/js/library.js
+++ src/js/library.js
@@ -35,95 +35,44 @@ watchedListButton.addEventListener('click', drawWatchedFilmList);
 //   return listItem;
 // };
 
-function drawQueueFilmList() {
+// function drawQueueFilmList() {
+//   let fragment = document.createDocumentFragment();
+//   let queueFilmListFromLS = localStorage.getItem('filmsQueue');
+//   if (queueFilmListFromLS !== null && JSON.parse(queueFilmListFromLS).length !== 0) {
+//     JSON.parse(queueFilmListFromLS).forEach(movie => {
+//       fragment.append(createOneObject(movie.poster_path, movie.title, movie.id, movie.vote_average))
+//     })
+//     libraryFilmList.innerHTML = "";
+//     libraryFilmList.append(fragment);
+//   } else if (queueFilmListFromLS === null || JSON.parse(queueFilmListFromLS).length === 0) {
+//     libraryFilmList.innerHTML = "";
+//     const listItem = document.createElement('li');
+//     listItem.classList.add('main__noFilmsInList');
+//     listItem.textContent = "You do not have to queue movies to watch. Add them."
+//     libraryFilmList.append(listItem);
+//   }
+ 
+// }
+
+function drawWatchedFilmList() {
   let fragment = document.createDocumentFragment();
-  let queueFilmListFromLS = localStorage.getItem('filmsQueue');
-  if (queueFilmListFromLS !== null && JSON.parse(queueFilmListFromLS).length !== 0) {
-    JSON.parse(queueFilmListFromLS).forEach(movie => {
-      fragment.append(createOneObject(movie.poster_path, movie.title, movie.id, movie.vote_average))
-    })
+  let watchedFilmListFromLS = localStorage.getItem('filmsWatched');
+  if (watchedFilmListFromLS !== null && JSON.parse(watchedFilmListFromLS).length !== 0) {
+    JSON.parse(watchedFilmListFromLS).forEach(movie => {
+      fragment.append(createOneObject(movie.poster_path, movie.title, movie.id))
+    });
     libraryFilmList.innerHTML = "";
     libraryFilmList.append(fragment);
-  } else if (queueFilmListFromLS === null || JSON.parse(queueFilmListFromLS).length === 0) {
+  }
+  else if (watchedFilmListFromLS === null || JSON.parse(watchedFilmListFromLS).length === 0) {
     libraryFilmList.innerHTML = "";
     const listItem = document.createElement('li');
     listItem.classList.add('main__noFilmsInList');
-    listItem.textContent = "You do not have to queue movies to watch. Add them."
+    listItem.textContent = "You do not have watched movies. Add them."
     libraryFilmList.append(listItem);
   }
- 
 }
 
-// function drawWatchedFilmList() {
-//   let fragment = document.createDocumentFragment();
-//   let watchedFilmListFromLS = localStorage.getItem('filmsWatched');
-//   if (watchedFilmListFromLS !== null && JSON.parse(watchedFilmListFromLS).length !== 0) {
-//     JSON.parse(watchedFilmListFromLS).forEach(movie => {
-//       fragment.append(createOneObject(movie.poster_path, movie.title, movie.id))
-//     });
-//     libraryFilmList.innerHTML = "";
-//     libraryFilmList.append(fragment);
-//   }
-//   // else if (watchedFilmListFromLS === null || JSON.parse(watchedFilmListFromLS).length === 0) {
-//   //   libraryFilmList.innerHTML = "";
-//   //   const listItem = document.createElement('li');
-//   //   listItem.classList.add('main__noFilmsInList');
-//   //   listItem.textContent = "You do not have watched movies. Add them."
-//   //   libraryFilmList.append(listItem);
-//   // }
-// }
-
-  // queueListButton.addEventListener('click', drawQueueFilmList);
-  // watchedListButton.addEventListener('click', drawWatchedFilmList);
-
-  
-const refs = {
-    gallery:document.querySelector('.gallery')
-}
-
-import { filmGenre } from './genre'
 
 
-const imageURL = "https://image.tmdb.org/t/p/w500";
-function createOneObject(film){
-  const { title, genre_ids, poster_path, release_date, id } = film;
-  const genreInCard = filmGenre(genre_ids);
-
-return `
-<li class="film-card" id = ${id}>
-  <a href=''>
-    <img class="poster" src="${imageURL}${poster_path}" alt="${title}"  />
-
-    <p class="info-title">${title}</p>
-    <div class="info-title info-item">
-      <p class="">${genreInCard}
-      </p>
-      <p class="">${release_date.slice(0,4)}
-      </p>
-    </div>
-  </a>
-</li>
-`};
-
-function generateMarkup (arrayImages){
-
-return arrayImages.reduce((acc,film)=>acc + createOneObject(film),"");
-};
-export  default function insertCreatedObject (arrayImages) {
-  const result = generateMarkup(arrayImages);
-  refs.gallery.innerHTML = result;
  
-
-};
-
-let listWatchedFilm = JSON.parse(localStorage.getItem('filmsWatched')) || [];
-
-    export default function addToWatchedFilm(data) {
-
-        let film = data;
-        if(data === "") {
-        return [];
-        }
-        listWatchedFilm.push(film)
-        localStorage.setItem('filmsWatched', JSON.stringify(listWatchedFilm)); 
-};
