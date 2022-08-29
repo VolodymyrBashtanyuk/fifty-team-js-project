690f3f8e1590f9fe6f160c4917e735ff24af6e7a
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-08-29 12:54:00 +0300

    fix/mobilePagination

--- src/js/createPagination.js
+++ src/js/createPagination.js
@@ -42,15 +42,15 @@ function createArr(start, end) {
 
 function renderPagination(elems, current, lastPage) {
     const markup = elems.map((index) => {
-        if (index === 0) {
-    return `<li ><div class = "empryBox">...</div></li>`    
-        }
-    
-    return `<li ><button type="button" class = "${index === current ? 'current' : ''}" data-btn="${index}">${index}</button></li>`
+        if (index === 0) return `<li class = "pagination__item"><div class = "empryBox">...</div></li>`    
+        if (index === 1) return `<li class = "pagination__item"><button type="button" class = "${index === current ? 'current' : ''} firstPage" data-btn="${index}">${index}</button></li>`
+        if (index === lastPage) return `<li class = "pagination__item"><button type="button" class = "${index === current ? 'current' : ''} lastPage" data-btn="${index}">${index}</button></li>`
+
+        return `<li class = "pagination__item"><button type="button" class = "${index === current ? 'current' : ''}" data-btn="${index}">${index}</button></li>`
     })
 
-    markup.unshift(`<li ><button type="button" class = "left-arrow" data-btn="<"></button></li>`)
-    markup.push(`<li ><button type="button" class = "right-arrow" data-btn=">"></button></li>`)
+    markup.unshift(`<li class = "pagination__item"><button type="button" class = "left-arrow" data-btn="<"></button></li>`)
+    markup.push(`<li class = "pagination__item"><button type="button" class = "right-arrow" data-btn=">"></button></li>`)
     paginationEl.innerHTML = markup.join('');
     paginationEl.dataset.lastpage = lastPage
 }
@@ -91,7 +91,7 @@ async function movies() {
         insertCreatedObject(response.results)
         createPagination(response)
         spinner.removeSpinner()
-        smoothScrool()
+        // smoothScrool()
 
     }catch(error){
         console.log(error)
