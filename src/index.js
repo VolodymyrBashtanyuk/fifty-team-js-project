62c33287b23632a657d87b17b1633843988e737c
Darya <daryabobur@gmail.com>
2022-08-29 18:47:02 +0300

    Merge branch 'main' into local-storage

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ import addToQueueFilm from "./js/localStorageToQueueFilm";
 import removeStorageWatchedFilm from './js/localStorageToWatchedFilm';
 import removeStorageQueueFilm from './js/localStorageToQueueFilm';
 
+
 async function movies(){
     spinner.startSpinner();
     try{
@@ -50,32 +51,34 @@ async function oneMovies(e) {
     try {
         const id = e.target.parentNode.parentNode.id;
         const oneMovieResponse = await movieDbApi.fetchOneMovie(id);
-    
+        
         createdCardFilm(oneMovieResponse);
         console.log(oneMovieResponse);
 
+        
     document.addEventListener('click', localStorageFilmData)
 
     function localStorageFilmData(evt) {
      const btn = evt.target;  
       
     if (evt.target.className === 'btn-watched') {
-        
+     
         addToWatchedFilm(oneMovieResponse);
+        
         btn.textContent = 'remove watched film';
-       
          console.log('Click!');
-         document.removeEventListener('click', localStorageFilmData);
+         console.log(oneMovieResponse.id);
+         
         } 
         else if(evt.target.className === 'btn-queue') {
          addToQueueFilm(oneMovieResponse);
          btn.textContent = 'remove queued film';
          console.log('Click!2');
          document.removeEventListener('click', localStorageFilmData)
+    
         }
-        
+    
 }
-       
     } catch(error) {
         console.log(error);
     };
