10872dc7ed44e45b5624c71afb916d7feb2e5266
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-08-30 16:04:30 +0300

    Update library.js

---
+++ src/library.js
@@ -0,0 +1,52 @@
+// import insertCreatedObject from "./js/createOneObject";
+import { filmGenre } from './js/genre'
+
+const libraryGallery = document.querySelector('.gallery')
+
+const imageURL = "https://image.tmdb.org/t/p/w500";
+
+console.log(libraryGallery);
+const filmsArr = getWatchedData()
+
+insertCreatedObject(filmsArr)
+
+function getWatchedData() {
+    try {
+        const watched = JSON.parse(localStorage.getItem('filmsWatched')) 
+        console.log(watched);
+        return watched
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function createOneObject(film){
+  const { title, genres, poster_path, release_date, id } = film;
+  const genreInCard = genres.map(ganre => ganre.name).join(' ');
+
+return `
+<li class="film-card" id = ${id}>
+  <a href=''>
+    <img class="poster" src="${imageURL}${poster_path}" alt="${title}"  />
+
+    <p class="info-title">${title}</p>
+    <div class="info-title info-item">
+      <p class="">${genreInCard}
+      </p>
+      <p class="">${release_date.slice(0,4)}
+      </p>
+    </div>
+  </a>
+</li>
+`};
+
+function generateMarkup (arrayImages){
+
+return arrayImages.reduce((acc,film)=>acc + createOneObject(film),"");
+};
+
+function insertCreatedObject (arrayImages) {
+  const result = generateMarkup(arrayImages);
+  libraryGallery.innerHTML = result;
+
+};
