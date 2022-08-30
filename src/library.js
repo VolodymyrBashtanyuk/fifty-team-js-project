81aa5da30b25ca43f8de16a213aad18c7d16e854
VOLODYMYR BASHTANYUK <96777965+VolodymyrBashtanyuk@users.noreply.github.com>
2022-08-31 01:03:01 +0300

    add correction in oneMovie

--- src/library.js
+++ src/library.js
@@ -10,6 +10,8 @@ import openCardFilm from './js/openCardFilm';
 
 import oneMovies from "./js/oneMovie";
 
+import addToWatchedFilm from "./js/localStorageToWatchedFilm";
+import addToQueueFilm from "./js/localStorageToQueueFilm";
 
 import onWatchedBtnClick from './js/library/renderWatchedData';
 import onQueueBtnClick from './js/library/renderQueueData';
