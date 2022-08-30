b864762527d7b23707b346959973ffb5b1a1994e
EnVy <envy7321@gmail.com>
2022-08-30 12:25:25 +0300

    save

--- src/js/createOneObject.js
+++ src/js/createOneObject.js
@@ -1,27 +1,27 @@
 const refs = {
-    gallery:document.querySelector('.gallery')
+  gallery: document.querySelector('.gallery')
 }
 
 import { filmGenre } from './genre'
 
-const noImg ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT6UB-bP8Q1VOTxmMLs-1v1d6LN4b7H7U3Ng&usqp=CAU';
+const noImg = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png';
 const imageURL = "https://image.tmdb.org/t/p/w500";
-function createOneObject(film){
+function createOneObject(film) {
   const { title, genre_ids, poster_path, release_date, id } = film;
   const genreInCard = filmGenre(genre_ids);
 
-return `
+  return `
 <li class="film-card" id = ${id}>
   <a href=''>
-  ${poster_path ? 
-    `<img class="poster" src="${imageURL}${poster_path}" alt="${title}"  />` : 
-    `<img class="poster" src=${noImg} alt="${title}" />`
-}
+  ${poster_path ?
+      `<img class="poster" src="${imageURL}${poster_path}" alt="${title}"  />` :
+      `<img class="poster" src=${noImg} alt="${title}" />`
+    }
     <p class="info-title">${title ?? ''}</p>
     <div class="info-title info-item">
       <p class="">${genreInCard ?? ''}
       </p>
-      <p class="">${release_date.slice(0,4) ?? ''}
+      <p class="">${release_date.slice(0, 4) ?? ''}
       </p>
     </div>
   </a>
@@ -30,14 +30,14 @@ return `
 
 
 
-function generateMarkup (arrayImages){
+function generateMarkup(arrayImages) {
 
-return arrayImages.reduce((acc,film)=>acc + createOneObject(film),"");
+  return arrayImages.reduce((acc, film) => acc + createOneObject(film), "");
 };
 
- export  default function insertCreatedObject (arrayImages) {
+export default function insertCreatedObject(arrayImages) {
   const result = generateMarkup(arrayImages);
   refs.gallery.innerHTML = result;
- 
+
 
 };
