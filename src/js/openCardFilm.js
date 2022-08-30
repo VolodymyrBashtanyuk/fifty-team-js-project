07f6b7e49f1f701d088ab8102b3d52af9a288051
Darya <daryabobur@gmail.com>
2022-08-30 22:53:33 +0300

    fix modal

--- src/js/openCardFilm.js
+++ src/js/openCardFilm.js
@@ -15,7 +15,7 @@ function openCardFilm (e) {
     if (!e.target.classList.contains('info-title') &&  !e.target.classList.contains('poster')) {
         return;
     } 
-
+refsModal.backdrop.classList.remove('hide');
 refsModal.backdrop.classList.add('is-open');
 document.addEventListener('keydown', closeModalEsc);
 document.removeEventListener('click', closeModalEsc);
@@ -42,3 +42,29 @@ function closeModalClick(e){
         document.removeEventListener('click', closeModalClick);
     };
 }
+
+
+
+// (() => {
+//     const refs = {
+//       closeModalBtn: document.querySelector("[data-modal-close]"),
+//       modal: document.querySelector("[data-modal]"),
+//     };
+  
+//     refs.openModalBtn.addEventListener("click", toggleModal);
+//     refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+//     function toggleModal() {
+//       document.body.classList.toggle("modal-open");
+//       refs.modal.classList.toggle("is-hidden");
+//     }
+//   })();
+
+const btnClose = document.querySelector('[data-modal-close]');
+btnClose.addEventListener('click', () => {
+    refsModal.backdrop.classList.add('hide');
+})
+
+// function closeBtn() {
+   
+// }
