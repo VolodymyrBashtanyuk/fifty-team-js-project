1515fddf932789cfb66deb653808161d6898859c
Natalia <72099196+Nataliia2@users.noreply.github.com>
2022-08-31 22:14:20 +0300

    правка console.log

--- src/js/localStorageToQueueFilm.js
+++ src/js/localStorageToQueueFilm.js
@@ -8,9 +8,7 @@ let listQueue = JSON.parse(localStorage.getItem('filmsQueue')) || [];
 export default function addToQueueFilm(data) {
 
     let film = data;
-    console.log(data);
-    // console.log(dataWatchedStorage);
-    // console.log(dataQueueStorage);
+
     if(data === "") {
     return [];
     }  
