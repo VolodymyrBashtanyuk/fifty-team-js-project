577402f36274ccb6bd30e4afbbfdad0046c37ae0
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-09-02 00:46:31 +0300

    fix

--- src/js/removeStorageQueueFilm.js
+++ src/js/removeStorageQueueFilm.js
@@ -1,11 +1,13 @@
 import getQueueData from './library/getQueueData';
+import { insertCreatedObject } from './library/insertMainMarkup';
 
 export default function removeStorageQueueFilm(id) {
   let listQueueFilm = getQueueData();
   const btnQueue = document.getElementById('btn-q');
   const btnRemoveQueueFilm = document.getElementById('btn-rq');
     const index = listQueueFilm.findIndex(film => film.id === id);
-    listQueueFilm.splice(index, 1);
+  listQueueFilm.splice(index, 1);
+  insertCreatedObject(listQueueFilm)
     localStorage.setItem('filmsQueue', JSON.stringify(listQueueFilm));
 btnQueue.classList.remove('hide');
 btnRemoveQueueFilm.classList.add('hide');
--- src/js/removeStorageWatchedFilm.js
+++ src/js/removeStorageWatchedFilm.js
@@ -1,11 +1,14 @@
 import getWatchedData from './library/getWatchedData';
+import { insertCreatedObject } from './library/insertMainMarkup';
+
 
 export default function removeStorageWatchedFilm(id) {
   let listWatchedFilm = getWatchedData();
   const btnWatched = document.getElementById('btn-w');
   const btnRemoveWatchedFilm = document.getElementById('btn-rw');
     const index = listWatchedFilm.findIndex(film => film.id === id);
-    listWatchedFilm.splice(index, 1);
+  listWatchedFilm.splice(index, 1);
+  insertCreatedObject(listWatchedFilm)
     localStorage.setItem('filmsWatched', JSON.stringify(listWatchedFilm));
     btnWatched.classList.remove('hide');
     btnRemoveWatchedFilm.classList.add('hide');
