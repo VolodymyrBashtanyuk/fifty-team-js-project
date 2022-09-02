ed675ec082bf63599c64a890f8b38f5b847c91aa
VOLODYMYR BASHTANYUK <96777965+VolodymyrBashtanyuk@users.noreply.github.com>
2022-09-02 17:55:43 +0300

    add innerHTML for search

--- src/js/keywordSearch.js
+++ src/js/keywordSearch.js
@@ -2,17 +2,25 @@ import { createPagination, saveQueryTypeLs } from "./createPagination";
 import { insertCreatedObject} from './createMainMarkup';
 import spinner from './preLoader';
 
+
 export const refs = {
   form: document.querySelector('.form'),
   falseresultMessage: document.querySelector('.falseresult__message'),
+  gallery: document.querySelector('.gallery'),
+  pagination: document.querySelector('.pagination'),
   }
 
 export function filterResults(searchMovie) {
+  spinner.startSpinner();
   if (searchMovie.total_results === 0) {
     refs.falseresultMessage.classList.remove('hide');
+    refs.gallery.innerHTML = '';
+    refs.pagination.innerHTML = '';
+    spinner.removeSpinner();
     return;
 } else if (searchMovie.total_results > 0){
     insertCreatedObject(searchMovie.results);
+    spinner.removeSpinner();
   }
   if (searchMovie.total_pages > 1) 
     saveQueryTypeLs('getOneMovie')
