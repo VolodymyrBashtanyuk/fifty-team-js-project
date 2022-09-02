ef4a7d4aa3e8e2fdc2b739200afc71805945c60a
VOLODYMYR BASHTANYUK <96777965+VolodymyrBashtanyuk@users.noreply.github.com>
2022-09-02 14:41:22 +0300

    bug fix

--- src/js/localStorageAddWatchedFilm.js
+++ src/js/localStorageAddWatchedFilm.js
@@ -1,11 +1,10 @@
-    export default function addToWatchedFilm(data) {
-        let listWatchedFilm = JSON.parse(localStorage.getItem('filmsWatched')) || [];
+export default function addToWatchedFilm(data) {
+    let listWatchedFilm = JSON.parse(localStorage.getItem('filmsWatched')) || [];
 
-        let film = data;
-        if(data === "") {
-        return [];
-        } 
-        listWatchedFilm.push(film)
-        localStorage.setItem('filmsWatched', JSON.stringify(listWatchedFilm));
-    };
-
+    let film = data;
+    if(data === "") {
+    return [];
+    } 
+    listWatchedFilm.push(film)
+    localStorage.setItem('filmsWatched', JSON.stringify(listWatchedFilm));
+};
